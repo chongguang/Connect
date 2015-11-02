49cfbf6ee7a2aca339bc090332e7956f1756915e
Chongguang <chongguang.liu@insa-lyon.fr>
2015-11-02 15:15:35 +0100

    create token class

---
+++ services/Token.js
@@ -0,0 +1,13 @@
+
+var Token = function(){
+	this.x = -1;
+	this.y = -1;
+	this.color = -1;
+
+};
+
+Token.prototype.print = function(){
+	console.log('hi');
+}
+
+module.exports = Token;
--- tests/tokenTest.js
+++ tests/tokenTest.js
@@ -1,6 +1,8 @@
-
+var Token = require('../services/Token.js');
 
 exports.getPosition = function(test){
+	var t1 = new Token();
+	t1.print();
 	test.equals("1", "1", "dfgdfgfd");
 
 	test.done();
