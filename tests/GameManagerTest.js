e3a8a1bdefe6e0c9f67a142e9ffcd0eb7aea9acb
Elyes Hachem <freezerhm@yahoo.fr>
2015-11-03 15:20:51 +0100

    Commit before pulling

--- tests/GameManagerTest.js
+++ tests/GameManagerTest.js
@@ -55,3 +55,25 @@ exports.switchPlayers = function(test){
 
 	test.done();
 };
+
+exports.gridIsFullAndTheirIsNoWinner = function(test){
+	var myBoard = new Board(5,5);
+	var player1 = new Player(myBoard,'*');
+	var player2 = new Player(myBoard,'o');
+	var myGameManager = new GameManager(player1, player2, myBoard);
+	for(var i = 0; i < myBoard.getRowSize(); i++){
+		for(var j = 0 ; j < (myBoard.getColumnSize()/4); j++){
+			var currentPlayer = myGameManager.whichPlayerHasToPlay();
+			currentPlayer.play(i);
+			currentPlayer.play(i);
+			myGameManager.nextPlayer();
+			currentPlayer = myGameManager.whichPlayerHasToPlay();
+			currentPlayer.play(i);
+			currentPlayer.play(i);
+			myGameManager.nextPlayer();
+		}
+	}
+	
+	myBoard.print();
+	test.done();
+};
