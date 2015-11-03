19e131b2829c9d6529a885402f38441a3ef4cdaf
Chongguang <chongguang.liu@insa-lyon.fr>
2015-11-03 15:19:40 +0100

    debuf checkwin of gamemanager

--- tests/GameManagerTest.js
+++ tests/GameManagerTest.js
@@ -55,3 +55,101 @@ exports.switchPlayers = function(test){
 
 	test.done();
 };
+
+
+exports.testAVerticalWinCase = function(test){
+	var myBoard = new Board(7,6);
+	var player1 = new Player(myBoard,'*');
+	var player2 = new Player(myBoard,'o');
+	var myGameManager = new GameManager(player1, player2, myBoard);
+
+	player1.play(1);
+	player1.play(1);
+	player1.play(1);
+	player1.play(1);
+
+	test.ok(myGameManager.checkWin(1), 'blabla');
+	test.done();
+
+}
+
+
+exports.testAHoriyontalWinCase = function(test){
+	var myBoard = new Board(7,6);
+	var player1 = new Player(myBoard,'*');
+	var player2 = new Player(myBoard,'o');
+	var myGameManager = new GameManager(player1, player2, myBoard);
+
+	player1.play(0);
+	player1.play(1);
+	player1.play(2);
+	player1.play(3);
+
+	test.ok(myGameManager.checkWin(0), 'blabla');
+	test.ok(myGameManager.checkWin(1), 'blabla');
+	test.ok(myGameManager.checkWin(2), 'blabla');
+	test.ok(myGameManager.checkWin(3), 'blabla');
+	test.done();
+
+}
+
+
+exports.testADiagnalLeftHighToRightLowWinCase = function(test){
+	var myBoard = new Board(7,6);
+	var player1 = new Player(myBoard,'*');
+	var player2 = new Player(myBoard,'o');
+	var myGameManager = new GameManager(player1, player2, myBoard);
+
+	player2.play(0);
+	player2.play(0);
+	player2.play(0);
+
+	player2.play(1);
+	player2.play(1);
+
+	player2.play(2);
+
+	player1.play(0);
+	player1.play(1);
+	player1.play(2);
+	player1.play(3);
+
+
+	test.ok(myGameManager.checkWin(0), 'blabla');
+	test.ok(myGameManager.checkWin(1), 'blabla');
+	test.ok(myGameManager.checkWin(2), 'blabla');
+	test.ok(myGameManager.checkWin(3), 'blabla');
+	test.done();
+
+}
+
+
+
+exports.testADiagnalLeftLowToRightHighWinCase = function(test){
+	var myBoard = new Board(7,6);
+	var player1 = new Player(myBoard,'*');
+	var player2 = new Player(myBoard,'o');
+	var myGameManager = new GameManager(player1, player2, myBoard);
+
+	player2.play(3);
+	player2.play(3);
+	player2.play(3);
+
+	player2.play(2);
+	player2.play(2);
+
+	player2.play(1);
+
+	player1.play(0);
+	player1.play(1);
+	player1.play(2);
+	player1.play(3);
+
+
+	test.ok(myGameManager.checkWin(0), 'blabla');
+	test.ok(myGameManager.checkWin(1), 'blabla');
+	test.ok(myGameManager.checkWin(2), 'blabla');
+	test.ok(myGameManager.checkWin(3), 'blabla');
+	test.done();
+
+}
